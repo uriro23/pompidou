39f91d91251dd8c07b9ec3a49bee7e2eb38fbe18
Uri <uri@rosenan.net>
2014-11-26 12:08:11 +0200

    Delete order modal complete

--- app/js/orderModals.js
+++ app/js/orderModals.js
@@ -4,6 +4,13 @@
 angular.module('myApp')
 .controller ('AckDelOrderCtrl', function($modalInstance, order) {
   this.order = order;
+  this.customer = order.view.customer;
+  this.days = Math.round((order.attributes.eventDate - new Date())/(24*3600*1000));
+  this.daysDirection = 'בעוד';
+  if (this.days<0) {
+    this.days = -this.days;
+    this.daysDirection = 'לפני';
+  }
 
   this.setYes = function() {
     $modalInstance.close (true);
