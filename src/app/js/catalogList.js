729a456d957ba936cb8c54e05515d192c806873a
uriro23 <uri@rosenan.net>
2017-08-12 11:04:52 +0300

    catalogList: optimization - query only requested category items

--- src/app/js/catalogList.js
+++ src/app/js/catalogList.js
@@ -22,27 +22,7 @@ angular.module('myApp')
       $state.go('editCatalogItem', {'id':id});
     };
 
-    this.sortCatalog = function (catalog) {
-      // sort results by category order and product description
-      // first build a hash of category order by tId (which is the value stored in category field of catalog)
-      var catOrder = [];
-      for (var i = 0; i < this.categories.length; i++) {
-        catOrder[this.categories[i].tId] = this.categories[i].order;
-      }
-      return catalog.sort(function (a, b) {
-        if (catOrder[a.attributes.category] > catOrder[b.attributes.category]) {
-          return 1;
-        } else if (catOrder[a.attributes.category] < catOrder[b.attributes.category]) {
-          return -1;
-        } else if (a.attributes.productDescription > b.attributes.productDescription) {
-          return 1;
-        } else {
-          return -1;
-        }
-      });
-    };
-
-    this.filterItems = function(){
+     this.filterItems = function(){
       var that = this;
       this.categoryItems = this.catalog.filter(function(cat) {
         return cat.attributes.category===that.currentCategory.tId &&
@@ -79,30 +59,36 @@ angular.module('myApp')
             that.currentCategory = that.categories.filter(function(cat) {
               return cat.tId === currentCategory;
             })[0];
-          return api.queryCatalog(that.currentDomain.id)
+          return api.queryCatalogByCategory(that.currentCategory.tId)
             .then(function (results) {
-              that.catalog = that.sortCatalog(results);
+              that.catalog = results.sort(function (a, b) {
+                if (a.attributes.productDescription > b.attributes.productDescription) {
+                  return 1;
+                } else {
+                  return -1;
+                }
+              });
               // enrich catalog data
-              for (var i = 0; i < that.catalog.length; i++) {
-                that.catalog[i].view = {};
-                that.catalog[i].view.category = that.categories.filter(function (cat) {
-                  return cat.tId === that.catalog[i].attributes.category;
+              that.catalog.forEach(function(cat) {
+                cat.view = {};
+                cat.view.category = that.categories.filter(function (categ) {
+                  return categ.tId === cat.attributes.category;
                 }) [0];
-                that.catalog[i].view.measurementUnit = that.measurementUnits.filter(function (mes) {
-                  return mes.tId === that.catalog[i].attributes.measurementUnit;
+                cat.view.measurementUnit = that.measurementUnits.filter(function (mes) {
+                  return mes.tId === cat.attributes.measurementUnit;
                 }) [0];
-                if (typeof that.catalog[i].attributes.minTimeUnit === 'number') {
-                  that.catalog[i].view.minTimeUnit = lov.timeUnits.filter(function (tu) {
-                    return tu.id === that.catalog[i].attributes.minTimeUnit;
+                if (typeof cat.attributes.minTimeUnit === 'number') {
+                  cat.view.minTimeUnit = lov.timeUnits.filter(function (tu) {
+                    return tu.id === cat.attributes.minTimeUnit;
                   }) [0];
                 }
-                if (typeof that.catalog[i].attributes.maxTimeUnit === 'number') {
-                  that.catalog[i].view.maxTimeUnit = lov.timeUnits.filter(function (tu) {
-                    return tu.id === that.catalog[i].attributes.maxTimeUnit;
+                if (typeof cat.attributes.maxTimeUnit === 'number') {
+                  cat.view.maxTimeUnit = lov.timeUnits.filter(function (tu) {
+                    return tu.id === cat.attributes.maxTimeUnit;
                   }) [0];
                 }
-                that.catalog[i].isChanged = false;
-              }
+                cat.isChanged = false;
+              });
               that.filterText = '';
               that.filterItems();
             });
