9b37a09727ca6b76974b84786c57fbe2d2eb319d
uriro23 <uri@rosenan.net>
2018-11-08 13:05:36 +0200

    catalogList: search items in whole domain

--- src/app/js/catalogList.js
+++ src/app/js/catalogList.js
@@ -24,8 +24,7 @@ angular.module('myApp')
      this.filterItems = function(){
       var that = this;
       this.categoryItems = this.catalog.filter(function(cat) {
-        return cat.attributes.category===that.currentCategory.tId &&
-          (cat.attributes.productName+' '+cat.attributes.productDescription).indexOf(that.filterText)>-1 &&
+        return (cat.attributes.productName+' '+cat.attributes.productDescription).indexOf(that.filterText)>-1 &&
             (that.includeDeleted || !cat.attributes.isDeleted);
       });
     };
@@ -45,6 +44,32 @@ angular.module('myApp')
           that.currentCategory = that.categories[0];
           that.setCategory();
         });
+    };
+
+    this.enrichCatalogData = function() {
+      var that = this;
+      this.catalog.forEach(function(cat) {
+        cat.view = {};
+        cat.view.category = that.categories.filter(function (categ) {
+          return categ.tId === cat.attributes.category;
+        }) [0];
+        cat.view.measurementUnit = that.measurementUnits.filter(function (mes) {
+          return mes.tId === cat.attributes.measurementUnit;
+        }) [0];
+        if (typeof cat.attributes.minTimeUnit === 'number') {
+          cat.view.minTimeUnit = lov.timeUnits.filter(function (tu) {
+            return tu.id === cat.attributes.minTimeUnit;
+          }) [0];
+        }
+        if (typeof cat.attributes.maxTimeUnit === 'number') {
+          cat.view.maxTimeUnit = lov.timeUnits.filter(function (tu) {
+            return tu.id === cat.attributes.maxTimeUnit;
+          }) [0];
+        }
+        cat.isChanged = false;
+      });
+      this.filterText = '';
+      this.filterItems();
     };
 
     this.fetchCategoryItems = function () {
@@ -68,33 +93,32 @@ angular.module('myApp')
                   return -1;
                 }
               });
-              // enrich catalog data
-              that.catalog.forEach(function(cat) {
-                cat.view = {};
-                cat.view.category = that.categories.filter(function (categ) {
-                  return categ.tId === cat.attributes.category;
-                }) [0];
-                cat.view.measurementUnit = that.measurementUnits.filter(function (mes) {
-                  return mes.tId === cat.attributes.measurementUnit;
-                }) [0];
-                if (typeof cat.attributes.minTimeUnit === 'number') {
-                  cat.view.minTimeUnit = lov.timeUnits.filter(function (tu) {
-                    return tu.id === cat.attributes.minTimeUnit;
-                  }) [0];
-                }
-                if (typeof cat.attributes.maxTimeUnit === 'number') {
-                  cat.view.maxTimeUnit = lov.timeUnits.filter(function (tu) {
-                    return tu.id === cat.attributes.maxTimeUnit;
-                  }) [0];
-                }
-                cat.isChanged = false;
-              });
-              that.filterText = '';
-              that.filterItems();
-            });
+              that.enrichCatalogData();
+          });
         });
     };
 
+    this.searchDomain = function () {
+      var that = this;
+      this.categoryItems = [];
+      api.queryCatalog(this.currentDomain.id)
+        .then(function(results) {
+          that.catalog = results.filter(function(cat) {
+            return (cat.attributes.productName+' '+cat.attributes.productDescription).indexOf(that.searchText)>-1
+          }).sort(function (a, b) {
+            if (a.attributes.category > b.attributes.category) {
+              return 1;
+            } else if (a.attributes.category < b.attributes.category) {
+              return -1
+            } else if (a.attributes.productName > b.attributes.productName) {
+              return 1;
+            } else {
+              return -1;
+            }
+          });
+          that.enrichCatalogData();
+        });
+    };
 
     // main block
     this.domains = angular.copy(lov.domains);  // clone so that the splice won't affect the original lov
